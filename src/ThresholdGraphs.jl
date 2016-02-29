239c311858e8a6c3fe0c53bcfe0c42467ee2ee8e
Ed Scheinerman <ers@jhu.edu>
2016-02-29 18:50:26 -0500

    Additional ThresholdGraph generator from Dict

--- src/ThresholdGraphs.jl
+++ src/ThresholdGraphs.jl
@@ -18,6 +18,36 @@ function ThresholdGraph{T<:Real}(w::Array{T,1})
 end
 
 """
+`ThresholdGraph(dw)` creates a threshold graph from a dictionary
+mapping vertices to weights.
+"""
+function ThresholdGraph{S,T<:Real}(dw::Dict{S,T})
+    G = SimpleGraph{S}()
+    vtcs = collect(keys(dw))
+    n = length(vtcs)
+    
+    for v in vtcs
+        add!(G,v)
+    end
+
+    for i=1:n-1
+        u = vtcs[i]
+        wt_u = dw[u]
+        for j=i+1:n
+            v = vtcs[j]
+            wt_v = dw[v]
+            if wt_u + wt_v >= 1
+                add!(G,u,v)
+            end
+        end
+    end
+    return G
+end
+    
+    
+
+
+"""
 `RandomThresholdGraph(n)` creates a random threshold graph with `n`
 vertices.
 """
