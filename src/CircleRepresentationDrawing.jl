83a0eed5835353bd0f502ff37cb8a66ca3be1708
Ed Scheinerman <ers@jhu.edu>
2016-04-09 07:00:30 -0400

    Added RainbowDrawing

--- src/CircleRepresentationDrawing.jl
+++ src/CircleRepresentationDrawing.jl
@@ -44,3 +44,43 @@ function CircleRepresentationDrawing(str::ASCIIString, labels::Bool=true)
   list = [ch for ch in str]
   CircleRepresentationDrawing(list,labels)
 end
+
+function semicircle(x1::Number, x2::Number)
+  ctr = (x1+x2)/2
+  rad = abs(x1-x2)/2
+
+  angles = collect(0:180)*(pi/180)
+  x = rad*[ cos(t) for t in angles ] + ctr
+  y = rad*[ sin(t) for t in angles ]
+  plot(x,y,"-",color="black")
+  plot(x[1],y[1],"ok")
+  plot(x[end],y[end],"ok")
+  axis("equal")
+  nothing
+end
+
+"""
+`RainbowDrawing(lst)` creates a rainbow representation
+of a circle graph from a list of the positions.
+"""
+function RainbowDrawing{T}(list::Array{T,1})
+  positions = SimpleGraphRepresentations.list2locs(list)
+  nn = length(list)
+  verts = collect(keys(positions))
+  clf()
+  gap = -0.25
+  for v in verts
+    p = positions[v]
+    semicircle(p[1],p[2])
+    text(p[1],gap,string(v))
+    text(p[2],gap,string(v))
+  end
+
+  x = [0,nn+1]
+  y = [0,0]
+  plot(x,y,"-",color="black")
+
+  axis("off")
+  axis("equal")
+  nothing
+end
