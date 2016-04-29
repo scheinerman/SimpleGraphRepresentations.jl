24e3f80418a2f49a67a9e2b98b4ab54c2a414d22
Ed Scheinerman <ers@jhu.edu>
2016-04-28 20:06:20 -0400

    Start working on T2 moves

--- src/CrossMatrix.jl
+++ src/CrossMatrix.jl
@@ -33,40 +33,84 @@ function CrossMatrix{T}(list::Array{T,1})
     return A
 end
 
-"""
-`find_example(n::Int)` looks for a circle representation whose cross
-join matrix has determinant equal to 1. Returns a triple:
-
-* `lst`: the representation
-* `A`: the cross join matrix
-* `G`: the corresponding circle graph
-```
-"""
-function find_example(n::Int)
-    if mod(n,2) == 1
-        error("n = $n is not even")
+function find_firsts_seconds{T}(list::Array{T,1})
+  F = Set{T}()  # firsts
+  n = length(list)
+  res = zeros(Int,n)
+  for i=1:n
+    x = list[i]
+    if in(x,F)
+      res[i]=2
+    else
+      push!(F,x)
+      res[i]=1
     end
-    while true
-        lst = RandomCircleRepresentation(n)
-        A = CrossMatrix(lst)
-        G = CircleGraph(lst)
-        if rank(A) == n
-            return lst,A,G
-        end
-    end
+  end
+  return res
 end
 
-"""
-`find_non_example(n)` is like `find_example`, except we want a
-singular cross join matrix.
-"""
-function find_non_example(n::Int)
-    while true
-        lst = RandomCircleRepresentation(n)
-        A = CrossMatrix(lst)
-        G = CircleGraph(lst)
-        if rank(A) < n && is_connected(G)
-            return lst,A,G
-        end
+
+function find_jumps{T}(list::Array{T,1})
+  n = length(list)
+  fs = find_firsts_seconds(list)
+
+  jumps = Int[]
+
+  for i=1:n-1
+    if fs[i] != fs[i+1] && list[i] != list[i+1]
+      push!(jumps,i)
     end
+  end
+  return jumps
+end
+
+# There's a spot at i,i+1 for a jump. Move the i to the right.
+function jump_over_right{T}(list::Array{T,1}, i::Int)
+  n = length(list)
+  fs = find_firsts_seconds(list)
+  if fs[i] == fs[i+1]
+    error("Not a valid jump location")
+  end
+
+  if fs[i] == 1
+    println("intersecting")
+    a = list[i+1]
+    b = list[i]
+    b_locs = find(list .== b)
+    j = b_locs[2]
+    idx = [ collect(1:i); collect(i+2:j-1); i+1; collect(j:n) ]
+  else
+    println("disjoint")
+    a = list[i]
+    b = list[i+1]
+    b_locs = find(list .== b)
+    j = b_locs[2]
+    idx = [ collect(1:i-1); collect(i+1:j); i; collect(j+1:n)]
+  end
+  println(idx)
+  return list[idx]
+end
+
+
+function build_jump_graph{T}(list::Array{T,1})
+  todo = []
+  done = Set()
+  push!(todo,list)
+  G = SimpleGraph()
+  add!(G,list)
+
+  while length(todo)>0
+    current = pop!(todo)
+    if in(current,done)
+      continue
+    end
+    jump_list = find_jumps(current)
+    for i in jump_list
+      next = jump_over_right(current,i)
+      add!(G,current,next)
+      push!(todo,next)
+    end
+    push!(done,current)
+  end
+  return G
 end
